abd522f27fc5383737849eb6bfcc78a2b10ca073
Aritra <aritranandi30@gmail.com>
2023-01-18 22:33:47 +0530

    Functions

---
+++ Functions/index.js
@@ -0,0 +1,59 @@
+// var x = 21;
+//         var girl = function () {
+//             console.log(x);
+//             var x = 20;
+//         };
+//         girl ();
+
+// var x = 21;
+//             girl ();
+//             console.log(x)
+//             function girl() {
+//                 console.log(x);
+//                 var x = 20;
+//             };
+
+
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+
+// function Sum(a,b){
+//     let s = a+b;
+//     console.log("sum= "+ s);
+// }
+// Sum(3,4);
+
+
+// function fact(n){
+//     let prod=1;
+//     for(let i=1; i<=n; i++){
+//         prod= prod*i;
+//     }
+//     console.log(prod);
+// }
+// fact(5);
+
+
+// function fact(n){
+//     let prod = 1, i=1;
+//     while(i<=n){
+//         prod = prod * i;
+//         i++;
+//     }
+    
+//     console.log(prod);
+// }
+// fact(7);
