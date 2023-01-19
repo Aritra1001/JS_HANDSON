d3588623753b115edb8b01f365b32a9ff9c3bc12
Aritra <aritranandi30@gmail.com>
2023-01-19 22:35:06 +0530

    functions

--- Functions/index.js
+++ Functions/index.js
@@ -57,3 +57,27 @@
 //     console.log(prod);
 // }
 // fact(7);
+
+// function FindSum(a, b){
+//     return a + b;
+//   } 
+
+//   function DisplayData(data, batch){
+//      console.log(`i am from ${data} and My batch is EA${batch}`)
+//   }
+
+//   DisplayData("PrepBytes", FindSum(10, 9));
+
+// Abc();
+//          const Abc = function(){
+//          let value = 20;
+//          console.log(value);
+//          }
+
+
+
+// var a = 10;
+// (function (){
+//     console.log(a);
+//     var a = 20;
+// })();
