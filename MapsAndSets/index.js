4fbd6bf5849d262927d0438776636f0b1370c559
Aritra <aritranandi30@gmail.com>
2023-01-20 18:56:12 +0530

    mapsandsets

---
+++ MapsAndSets/index.js
@@ -0,0 +1,39 @@
+// function removeDuplicate(str,n){
+//     var s = new Set();
+//     for(let i = 0; i < n; i++){
+//         s.add(str[i]);
+//     }
+//     for(const element of s){
+//         console.log(element);
+//     }
+// }
+// var str = "abcadeecfb"
+// var n = str.length;
+// removeDuplicate(str,n);
+
+function prCharWithFreq(s)
+{
+ 
+  var d = new Map();
+ 
+  s.split('').forEach(element => {
+     
+        if(d.has(element))
+        {
+            d.set(element, d.get(element)+1);
+        }
+        else
+            d.set(element, 1);
+  });
+ 
+  s.split('').forEach(element => {
+    if(d.has(element) && d.get(element)!=0)
+    {
+      console.log( element + d.get(element) + " ");
+      d.set(element, 0);
+    }
+  });
+}
+
+var s="abcadeecfb";
+prCharWithFreq(s);
